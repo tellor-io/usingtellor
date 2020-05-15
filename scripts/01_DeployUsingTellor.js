fd706aa83b30c10db0dd4f2f70711e145638e567
Brenda <l.loya.b@gmail.com>
2020-05-15 12:27:08 -0400

    deployment script

--- scripts/01_DeployUsingTellor.js
+++ scripts/01_DeployUsingTellor.js
@@ -1,11 +1,10 @@
 
 /**
 * @title Deploy User Contracts 
-* @dev This allows Tellor deploy the community sale contract
+* @dev This allows Tellor deploy the susingTellor contract
 */
 
 /*Imports*/
-var UserContract = artifacts.require("UserContract");
 var UsingTellor = artifacts.require("UsingTellor");
 var OracleIDDescriptions = artifacts.require("OracleIDDescriptions");
 
@@ -20,10 +19,6 @@ var HDWalletProvider = require("@truffle/hdwallet-provider");
 var web3 = new Web3(new HDWalletProvider('3a10b4bc1258e8bfefb95b498fb8c0f0cd6964a811eabca87df5630bcacd7216',"https://rinkeby.infura.io/v3/7f11ed6df93946658bf4c817620fbced"));
 //var web3 = new Web3(new HDWalletProvider("","https://mainnet.infura.io/v3/bc3e399903ae407fa477aa0854a00cdc"));
 
-/*notes for validating contract
-//solc: 0.5.8+commit.23d335f2.Emscripten.clang
-// truffle-flattener ./contracts/01_DeploySaleContract.sol > ./flat_files/01_DeploySaleContract.sol
-// truffle exec scripts/01_DeployTellor.js --network rinkeby
 
 /*Variables*/
 //rinkeby
@@ -41,8 +36,6 @@ var adjFactor2 = 1e0;
 //tellorMaster = '0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5';
 
 var api = "json(https://api.gdax.com/products/BTC-USD/ticker).price";
-//var bytes = web3.utils.keccak256(api, 1000);
-//console.log("bytes", bytes);
 console.log("start");
 
 
@@ -51,54 +44,47 @@ module.exports =async function(callback) {
     let oracleIDDescriptions;
    console.log("1")
 
-    oa = (web3.utils.toChecksumAddress(tellorMaster));
-    userContract = await UserContract.new(oa);
-    console.log("userContract address:", userContract.address);
+    oa = (web3.utils.toChecksumAddress(tellorMaster))
+
+    usingTellor = await UsingTellor.new(oa)
+
+    console.log("using tellor", usingTellor.address)
     sleep_s(30)
 
-
-//a = ''
-//userContract = await UserContract.at(a);
-
-    usingTellor = await UsingTellor.new(userContract.address)
-
-    console.log("using tellor", usingTellor.address);
+    oracleIDDesc = await OracleIDDescriptions.new()
+    console.log("oracleIDDesc address:", oracleIDDesc.address)
     sleep_s(30)
 
-    oracleIDDesc = await OracleIDDescriptions.new();
-    console.log("oracleIDDesc address:", oracleIDDesc.address);
-    sleep_s(30)
-
-    await userContract.setOracleIDDescriptors(oracleIDDesc.address);
-    console.log("user contract setOracleIdDescriptors address");
+    await userContract.setOracleIDDescriptors(oracleIDDesc.address)
+    console.log("user contract setOracleIdDescriptors address")
     sleep_s(30)
     
 
     //let ad = ""
-    //let oracleIDDesc = await OracleIDDescriptions.at(ad);
-    await oracleIDDesc.defineTellorCodeToStatusCode(0,400);
+    //let oracleIDDesc = await OracleIDDescriptions.at(ad)
+    await oracleIDDesc.defineTellorCodeToStatusCode(0,400)
     console.log("status code 0")
     sleep_s(30)
-    await oracleIDDesc.defineTellorCodeToStatusCode(1,200);
+    await oracleIDDesc.defineTellorCodeToStatusCode(1,200)
     console.log("status code 1")
     sleep_s(30)
-    await oracleIDDesc.defineTellorCodeToStatusCode(2,404);
+    await oracleIDDesc.defineTellorCodeToStatusCode(2,404)
     console.log("status code 2")
     sleep_s(30)
 
 
-    await oracleIDDesc.defineTellorIdToBytesID(tellorEthid,adoEthid);
-    console.log("defineTellorIdtoBytesId");
-    await oracleIDDesc.defineTellorIdToBytesID(tellorBtcid,adoBtcid);
-    console.log("defineTellorIdtoBytesId");
+    await oracleIDDesc.defineTellorIdToBytesID(tellorEthid,adoEthid)
+    console.log("defineTellorIdtoBytesId")
+    await oracleIDDesc.defineTellorIdToBytesID(tellorBtcid,adoBtcid)
+    console.log("defineTellorIdtoBytesId")
 
     await oracleIDDesc.defineTellorIdtoAdjFactor(tellorEthid, adjFactor1)
-    console.log("defineTellorIdtoGranularity");
+    console.log("defineTellorIdtoGranularity")
 
     await oracleIDDesc.defineTellorIdtoAdjFactor(tellorBtcid, adjFactor2)
-    console.log("defineTellorIdtoGranularity");
+    console.log("defineTellorIdtoGranularity")
 
-    await userContract.setPrice(web3.utils.toWei(".03","ether"));
+    await userContract.setPrice(web3.utils.toWei(".03","ether"))
     console.log("userContract set Price ")
 
 
