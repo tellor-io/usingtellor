6a2cff994f9c7da3daf7ab21c0621acdd9f76262
Tally Wiesenberg <t.j.wies@gmail.com>
2021-05-26 14:01:09 -0400

    delete unused functions

--- contracts/TellorPlayground.sol
+++ contracts/TellorPlayground.sol
@@ -9,6 +9,17 @@ library SafeMath {
         return c;
     }
 
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
 }
 
 contract TellorPlayground {
