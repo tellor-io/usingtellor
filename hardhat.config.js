702d536e040a03b831f5519efd14d1a133f8466c
Quentin <thealmightyquinn@hotmail.com>
2022-05-12 12:11:58 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,17 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- require("@nomiclabs/hardhat-waffle");
- require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.3",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+  },
 };
